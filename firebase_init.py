2d029b2baec07b0588247153aff285e2690f2384
santu-santu8 <nayanasantosh.23.cse@anits.edu.in>
2026-02-06 19:35:54 +0530

    Fix Firebase init to use environment secrets

--- firebase_init.py
+++ firebase_init.py
@@ -1,10 +1,22 @@
+import os
 import firebase_admin
-from firebase_admin import credentials, firestore, auth
+from firebase_admin import credentials, firestore
 
 def init_firebase():
     if not firebase_admin._apps:
-        cred = credentials.Certificate("firebase_key.json")
+        cred = credentials.Certificate({
+            "type": os.getenv("FIREBASE_TYPE"),
+            "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+            "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+            "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+            "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+            "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+            "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+            "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+            "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+            "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL"),
+        })
+
         firebase_admin.initialize_app(cred)
 
-    db = firestore.client()
-    return db
+    return firestore.client()
