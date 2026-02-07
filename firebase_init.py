151079fef962133a2b1d3e8ae30cde9819b4eefd
santu-santu8 <nayanasantosh.23.cse@anits.edu.in>
2026-02-07 12:02:51 +0530

    Remove file-based Firebase init, use env secrets only

--- firebase_init.py
+++ firebase_init.py
@@ -1,12 +1,22 @@
+import os
 import firebase_admin
 from firebase_admin import credentials, firestore
-import os
 
 def init_firebase():
     if not firebase_admin._apps:
-        # Relative path to the JSON in the project folder
-        cred_path = os.path.join(os.path.dirname(__file__), "firebase_key.json")
-        cred = credentials.Certificate(cred_path)
+        cred = credentials.Certificate({
+            "type": os.getenv("FIREBASE_TYPE"),
+            "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+            "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+            "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+            "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+            "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+            "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+            "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+            "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+            "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL"),
+        })
+
         firebase_admin.initialize_app(cred)
 
     return firestore.client()
